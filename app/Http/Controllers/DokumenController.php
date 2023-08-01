0f0a1d77e14c8016b041b72dc52d72311bd54c3e
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-01 11:57:43 +0700

    add DokumenController

---
+++ app/Http/Controllers/DokumenController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\TipeDokumenKendaraan;
+
+class DokumenController extends Controller
+{
+    public function index() {
+        $data = TipeDokumenKendaraan::all();
+
+        return view('dokumen.index', ['data' => $data]);
+    }
+
+    public function create(Request $request) {
+        TipeDokumenKendaraan::create([
+            'nama_dokumen' => $request->nama
+        ]);
+
+        return redirect('/tipe-dokumen');
+    }
+
+    public function show($id) {
+        $data = TipeDokumenKendaraan::find($id);
+
+        return view('dokumen.update', ['data' => $data]);
+    }
+
+    public function update($id, Request $request) {
+        TipeDokumenKendaraan::where('id', $id)->update([
+            'nama_dokumen' => $request->nama
+        ]);
+
+        return redirect('/tipe-dokumen');
+    }
+
+    public function del($id) {
+        TipeDokumenKendaraan::where('id', $id)->delete();
+
+        return redirect('/tipe-dokumen');
+    }
+}
