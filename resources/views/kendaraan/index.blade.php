5a5de69fe5902be8fb719286817af420562ee420
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-01 13:58:36 +0700

    add view for kendaraan

--- resources/views/kendaraan/index.blade.php
+++ resources/views/kendaraan/index.blade.php
@@ -15,7 +15,7 @@
             </div>
         </div> --}}
         <div class="card-body">
-            <button type="button" class="btn btn-primary">Tambah data</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCreate">Tambah data</button>
             <hr>
             <table class="table table-bordered">
                 <thead>
@@ -28,7 +28,7 @@
                         <th>Aksi</th>
                     </tr>
                 </thead>
-                <tbody>
+                {{-- <tbody>
                     <tr>
                         <td>1</td>
                         <td>B 123 A</td>
@@ -73,8 +73,63 @@
                             <button type="button" class="btn btn-danger">Delete</button>
                         </td>
                     </tr>
+                </tbody> --}}
+                <tbody>
+                    @foreach ($data as $d)
+                        <tr>
+                            <td>{{ $d->id }}</td>
+                            <td>{{ $d->nopol}}</td>
+                            <td>{{ $d->merk }}</td>
+                            <td>{{ $d->jenis_kendaraan }}</td>
+                            <td>{{ $d->warna }}</td>
+                            <td class="text-center">
+                                <a href="/kendaraan/{{ $d->id }}/update"><button type="button" class="btn btn-warning">Update</button></a>
+                                <a href="/kendaraan/{{ $d->id }}/delete"><button type="button" class="btn btn-danger">Delete</button></a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalCreate" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Tambah data baru</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form action="/kendaraan" method="POST">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Nomor Polisi</label>
+                            <input type="text" class="form-control" name="nopol" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Merk</label>
+                            <input type="text" class="form-control" name="merk" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Jenis kendaraan</label>
+                            <select class="form-control" name="jenis_kendaraan">
+                                <option value="Motor">Motor</option>
+                                <option value="Mobil">Mobil</option>
+                                <option value="Van">Van</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Warna</label>
+                            <input type="text" class="form-control" name="warna" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @stop
---
+++ resources/views/kendaraan/update.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title', 'Tambah Data Kendaraan')
+
+@section('content_header')
+    <h1>Update Data Kendaraan</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <form action="/kendaraan/{{ $data->id }}/update" method="POST">
+            {{ csrf_field() }}
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="nomorPolisi">Nomor Polisi</label>
+                    <input type="text" class="form-control" id="nomorPolisi" name="nopol" value="{{ $data->nopol }}">
+                </div>
+                <div class="form-group">
+                    <label for="merk">Merk</label>
+                    <input type="text" class="form-control" id="merk" name="merk" value="{{ $data->merk }}">
+                </div>
+                <div class="form-group">
+                    <label for="jenisKendaraan">Jenis Kendaraan</label>
+                    <select class="form-control" name="jenis_kendaraan">
+                        <option selected={{ $data->jenis_kendaraan === 'Motor' ? 1 : 0}}>Motor</option>
+                        <option selected={{ $data->jenis_kendaraan === 'Mobil' ? 1 : 0}}>Mobil</option>
+                        <option selected={{ $data->jenis_kendaraan === 'Van' ? 1 : 0}}>Van</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="warna">Warna</label>
+                    <input type="text" class="form-control" id="warna" name="warna" value="{{ $data->warna }}">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-success">Simpan</button>
+            </div>
+        </form>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
