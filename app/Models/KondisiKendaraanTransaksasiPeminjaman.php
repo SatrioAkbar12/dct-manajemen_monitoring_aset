0ecae0174f02b9922d9e9c0a860f0dd9f15c55eb
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-01 15:54:34 +0700

    add softdeletes in models

--- app/Models/KondisiKendaraanTransaksasiPeminjaman.php
+++ app/Models/KondisiKendaraanTransaksasiPeminjaman.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KondisiKendaraanTransaksasiPeminjaman extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'kondisi_kendaraan_transaksi_peminjaman';
 
--- app/Models/MasaAktifDokumenKendaraan.php
+++ app/Models/MasaAktifDokumenKendaraan.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MasaAktifDokumenKendaraan extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'masa_aktif_dokumen_kendaraan';
 
