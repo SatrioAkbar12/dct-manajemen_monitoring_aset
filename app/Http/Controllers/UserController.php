cf8666cc36b9ee38c4683c03926a6b37b5db9fdc
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-01 15:30:22 +0700

    add UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index() {
+        $data = User::all();
+
+        return view('user.index', ['data' => $data]);
+    }
+
+    public function store(Request $request) {
+        // return $request->memiliki_sim
+
+        User::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'password' => $request->password,
+            'memiliki_sim' => $request->memiliki_sim == 'on' ? 1 : 0
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function show($id) {
+        $data = User::find($id);
+
+        return view('user.update', ['data' => $data]);
+    }
+
+    public function update($id, Request $request) {
+        User::where('id', $id)->update([
+            'nama' => $request->nama,
+            'username' => $request->username,
+            'email' => $request->email,
+            'memiliki_sim' => $request->memiliki_sim == 'on' ? 1 : 0
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function del($id) {
+        User::where('id', $id)->delete();
+
+        return redirect('/user');
+    }
+}
