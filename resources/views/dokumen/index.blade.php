609656401dc953ed6dbc07fe49e5665dedc99d00
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-01 11:57:27 +0700

    add view for dokumen

---
+++ resources/views/dokumen/index.blade.php
@@ -0,0 +1,166 @@
+@extends('adminlte::page')
+
+@section('title', 'Master Data Jenis Dokumen Kendaraan')
+
+@section('content_header')
+    <h1>Master Data Tipe Dokumen Kendaraan</h1>
+@stop
+
+@section('content')
+    <p>Semua data tentang tipe dokumen kendaraan</p>
+
+    <div class="card">
+        {{-- <div class="card-header">
+            <div class="card-title">
+            </div>
+        </div> --}}
+        <div class="card-body">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCreate">Tambah data</button>
+            <hr>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nama Dokumen</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                {{-- <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>STNK</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalUpdate">Update</button>
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>BPKB</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-warning">Update</button>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Pajak tahunan</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-warning">Update</button>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody> --}}
+                <tbody>
+                    @foreach ($data as $d)
+                        <tr>
+                            <td>{{ $d->id }}</td>
+                            <td>{{ $d->nama_dokumen }}</td>
+                            <td class="text-center">
+                                {{-- <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalUpdate">Update</button> --}}
+                                <a href="/tipe-dokumen/{{ $d->id }}/update"><button type="button" class="btn btn-warning">Update</button></a>
+                                {{-- <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete">Delete</button> --}}
+                                <a href="/tipe-dokumen/{{ $d->id }}/delete"><button type="button" class="btn btn-danger">Delete</button></a>
+                                {{-- <button type="button" class="btn btn-primary dropdown-toggle waves-effect waves-light" data-toggle="modal" data-target="#myModal" data-community="{{ json_encode($d) }}">Edit</button>
+                                <a href="{{ URL::to('delete', array($d->id)) }}" class="btn btn-danger dropdown-toggle waves-effect waves-light">Delete</a></td> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {{-- <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Edit: <span></span></h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-group" method="post" id="editCommunityForm">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="text" name="community_name" class="form-control">
+                    </form>
+                    <button class="btn btn-custom dropdown-toggle waves-effect waves-light" type="submit" form="editCommunityForm">Update Community</button><br /><br />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+    <div class="modal fade" id="modalCreate" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Tambah data baru</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form action="/tipe-dokumen" method="POST">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" class="form-control" name="nama" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalUpdate" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Update data</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalDelete" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Hapus data</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah ingin menghapus data?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Tidak</button>
+                    {{-- <a href="/tipe-dokumen/{{ $data->id }}/delete/"> --}}
+                        <button type="button" class="btn btn-primary">Ya</button>
+                    {{-- </a> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
---
+++ resources/views/dokumen/update.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+@section('title', 'Tambah Data Tipe Dokumen')
+
+@section('content_header')
+    <h1>Update Tipe Dokumen Kendaraan</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <form action="/tipe-dokumen/{{ $data->id }}/update" method="POST">
+            {{ csrf_field() }}
+            {{-- {{ method_field('PUT') }} --}}
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="nomorPolisi">Nama</label>
+                    <input type="text" class="form-control" id="nomorPolisi" name="nama" value="{{ $data->nama_dokumen }}">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-success">Simpan</button>
+            </div>
+        </form>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
