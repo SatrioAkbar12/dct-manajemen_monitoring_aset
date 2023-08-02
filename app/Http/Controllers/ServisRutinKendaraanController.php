6429c16040e81e189c9cbfdf1803abc7c7d9e4b4
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-02 15:11:00 +0700

    add new function in ServisRutinKendaraanController

--- app/Http/Controllers/ServisRutinKendaraanController.php
+++ app/Http/Controllers/ServisRutinKendaraanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kendaraan;
+use App\Models\ServisRutinKendaraan;
 use Illuminate\Http\Request;
 
 class ServisRutinKendaraanController extends Controller
@@ -11,5 +12,29 @@ class ServisRutinKendaraanController extends Controller
         $kendaraan = Kendaraan::all();
 
         return view('servisRutin.index', ['data_kendaraan' => $kendaraan]);
+    }
+
+    public function getKendaraan($id_kendaraan) {
+        $kendaraan = Kendaraan::find($id_kendaraan);
+        $servis = ServisRutinKendaraan::where('id_kendaraan', $id_kendaraan)->orderBy('tanggal_servis', 'desc')->get();
+        $jumlah_servis = count($servis);
+
+        return view('servisRutin.show', ['data_kendaraan' => $kendaraan, 'data_servis' => $servis, 'jumlah_servis' => $jumlah_servis]);
+    }
+
+    public function store($id_kendaraan, Request $request) {
+        ServisRutinKendaraan::create([
+            'id_kendaraan' => $id_kendaraan,
+            'tanggal_servis' => $request->tanggal_servis,
+            'penggantian_oli' => $request->penggantian_oli == 'on' ? 1 : 0,
+            'cek_aki' => $request->cek_aki == 'on' ? 1 : 0,
+            'cek_rem' => $request->cek_rem == 'on' ? 1 : 0,
+            'cek_kopling' => $request->cek_kopling == 'on' ? 1 : 0,
+            'cek_ban' => $request->cek_ban == 'on' ? 1 : 0,
+            'cek_lampu' => $request->cek_lampu == 'on' ? 1 : 0,
+            'cek_ac' => $request->cek_ac == 'on' ? 1 : 0
+        ]);
+
+        return redirect('/servis-rutin/' . $id_kendaraan);
     }
 }
