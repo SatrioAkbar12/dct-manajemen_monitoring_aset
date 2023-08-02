30c21f93162a3452e7dcc0107dc0012e03494eeb
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-02 16:58:17 +0700

    fix error table name and add relations for transaksi peminjaman model

--- app/Models/TransaksiPeminjaman.php
+++ app/Models/TransaksiPeminjaman.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransaksiPeminjaman extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table = 'Transaksi Peminjaman';
+    protected $table = 'transaksi_peminjaman';
 
     protected $fillable = [
         'id_kendaraan',
@@ -18,4 +19,12 @@ class TransaksiPeminjaman extends Model
         'target_tanggal_waktu_kembali',
         'aktif',
     ];
+
+    public function kendaraan(): BelongsTo {
+        return $this->belongsTo(Kendaraan::class, 'id_kendaraan', 'id');
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
 }
