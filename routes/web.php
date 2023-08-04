f23d830cb75d31318080d04b8613d43fd9349c55
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-04 13:53:00 +0700

    update routing of kendaraan

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,9 @@ Route::middleware('auth')->controller(UserController::class)->prefix('user')->na
 Route::middleware('auth')->controller(KendaraanController::class)->prefix('kendaraan')->name('kendaraan.')->group(function() {
     Route::get('/', 'index')->name('index');
     Route::post('/', 'store')->name('store');
-    Route::get('/{id}/update', 'show')->name('show');
-    Route::post('/{id}/update', 'update')->name('update');
-    Route::get('/{id}/delete', 'del')->name('del');
+    Route::get('/{id}', 'show')->name('show');
+    Route::post('/{id}', 'update')->name('update');
+    Route::post('/{id}/delete', 'del')->name('del');
 });
 
 Route::middleware('auth')->controller(DokumenController::class)->prefix('tipe-dokumen')->name('tipeDokumen.')->group(function() {
