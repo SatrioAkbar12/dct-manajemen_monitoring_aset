918ebd7c6b21b19fcaf6ef6236f290d85a38092d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-07 11:23:38 +0700

    add MasaAktifDokumenRequest

---
+++ app/Http/Requests/MasaAktifDokumenRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rule;
+
+class MasaAktifDokumenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'tipe_dokumen' => [
+                'required',
+                'exists:App\Models\TipeDokumenKendaraan,id',
+                Rule::excludeIf(Route::currentRouteName() == 'masaAktifDokumen.update'),
+            ],
+            'masa_aktif' => 'required|date',
+        ];
+    }
+}
