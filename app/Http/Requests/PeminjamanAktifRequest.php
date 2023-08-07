622574681b5315185d26bb36c33567647d3fb6b2
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-07 12:05:37 +0700

    add PeminjamanAktifRequest

---
+++ app/Http/Requests/PeminjamanAktifRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
+
+class PeminjamanAktifRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        if(Route::currentRouteName() == 'peminjamanAktif.store') {
+            return [
+                'user' => 'required|exists:App\Models\User,id',
+                'kendaraan' => 'required|exists:App\Models\Kendaraan,id',
+                'target_tanggal_waktu_kembali' => 'required|date'
+            ];
+        }
+        elseif(Route::currentRouteName() == 'peminjamanAktif.update') {
+            return [
+                'status_kondisi' => 'required|in:Aman,Ada kerusakan',
+                'deskripsi' => 'required|string',
+                'foto_kondisi' => 'required|image',
+            ];
+        }
+    }
+}
