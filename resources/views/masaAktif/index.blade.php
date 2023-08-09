7f198e590724c517ffba7cbe4fad7b360f446935
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-09 19:10:11 +0700

    fix bug update jenis kendaraan on masa aktif dokumen

--- resources/views/masaAktif/index.blade.php
+++ resources/views/masaAktif/index.blade.php
@@ -27,7 +27,7 @@
                             <tr>
                                 <td>{{ $kendaraan->id }}</td>
                                 <td>{{ $kendaraan->nopol }}</td>
-                                <td>{{ $kendaraan->jenis_kendaraan . " " . $kendaraan->merk . " " . $kendaraan->warna }}</td>
+                                <td>{{ $kendaraan->jenisKendaraan->nama . " " . $kendaraan->merk . " " . $kendaraan->warna }}</td>
                                 <td class="text-danger">
                                     <ul>
                                         @if($kendaraan->masaAktifDokumen->count() == 0)
