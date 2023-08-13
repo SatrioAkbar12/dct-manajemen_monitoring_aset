523d38ff3279ae637ce200d6563168da8fab1b94
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-13 13:19:14 +0700

    add id_aset on kendaraan table

---
+++ database/migrations/2023_08_13_052805_add_id_aset_kendaraan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('kendaraan', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_aset');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('kendaraan', function (Blueprint $table) {
+            $table->dropColumn('id_aset');
+        });
+    }
+};
