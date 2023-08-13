cd2daee3b7fe419b698bb2a6f09f4a694872395a
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-13 13:26:37 +0700

    fix relation type aset to kendaraan

--- app/Models/Aset.php
+++ app/Models/Aset.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Aset extends Model
@@ -17,7 +17,7 @@ class Aset extends Model
         'kode_aset',
     ];
 
-    public function kendaraan(): HasMany {
-        return $this->hasMany(Kendaraan::class, 'id_aset', 'id');
+    public function kendaraan(): HasOne {
+        return $this->hasOne(Kendaraan::class, 'id_aset', 'id');
     }
 }
