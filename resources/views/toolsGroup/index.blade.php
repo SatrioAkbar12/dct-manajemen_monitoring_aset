59c54da4c1727f850724b9ebcf139f4ac9708dab
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-14 10:49:27 +0700

    add tools group controller, views, and route

---
+++ resources/views/toolsGroup/index.blade.php
@@ -0,0 +1,111 @@
+@extends('adminlte::page')
+
+@section('title', 'Master Data Tools Group')
+
+@section('content_header')
+    <h1>Master Data Tools Group</h1>
+@stop
+
+@section('content')
+    <p>Semua data tools group yang ada untuk mengelompokkan tools</p>
+
+    <div class="card">
+        <div class="card-body">
+            @can('toolsGroup.store')
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCreate">Tambah data</button>
+                <hr>
+            @endcan
+
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="text-center">
+                        <tr>
+                            <th>Id</th>
+                            <th>Nama grup</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data_grup as $grup)
+                            <tr>
+                                <td class="text-center">{{ $grup->id }}</td>
+                                <td>{{ $grup->nama }}</td>
+                                <td class="text-center">
+                                    @can('toolsGroup.update')
+                                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalUpdate{{ $grup->id }}">Update</button>
+                                    @endcan
+                                    @can('toolsGroup.del')
+                                        <a href="{{ route('toolsGroup.del', $grup->id) }}" class="btn btn-danger" data-confirm-delete="true">Hapus</a>
+                                    @endcan
+                                </td>
+                            </tr>
+
+                            @can('toolsGroup.update')
+                                <div class="modal fade" id="modalUpdate{{ $grup->id }}" role="dialog">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Update data</h4>
+                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            </div>
+                                            <form action="{{ route('toolsGroup.update', $grup->id) }}" method="POST">
+                                                {{ csrf_field() }}
+                                                <div class="modal-body">
+                                                    <div class="form-group">
+                                                        <label>Nama</label>
+                                                        <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value="{{ $grup->nama }}" required>
+                                                        @error('nama')
+                                                            <div class="text-danger">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
+                                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endcan
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <br>
+            <div class="d-flex justify-content-center">
+                {{ $data_grup->links() }}
+            </div>
+
+            @can('toolsGroup.store')
+                <div class="modal fade" id="modalCreate" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Tambah Data Baru</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+                            <form action="{{ route('toolsGroup.store') }}" method="POST">
+                                {{ csrf_field() }}
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label>Nama grup</label>
+                                        <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value={{ old('nama') }}>
+                                        @error('nama')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endcan
+        </div>
+    </div>
+@stop
