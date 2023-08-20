357c65892bcdd22bdc2c1002007b141959ef37b2
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-20 09:40:40 +0700

    update returning views on peminjaman aktif tools

--- resources/views/peminjamanAktifTool/returning.blade.php
+++ resources/views/peminjamanAktifTool/returning.blade.php
@@ -19,23 +19,53 @@
                         @endforeach
                     </ul>
                 </div>
+                <div class="form-group">
+                    <label>Kembali di gudang</label>
+                    <select class="form-control @error('gudang') is-invalid @enderror" id="inputGudang" name="gudang">
+                        @foreach ($data_gudang as $gudang)
+                            <option value="{{ $gudang->id }}">{{ $gudang->nama }}</option>
+                        @endforeach
+                    </select>
+                    @error('gudang')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
                 @foreach ($data_peminjaman_aktif->listTools as $listTools)
                     <div class="my-5">
                         <p class="lead">Kondisi {{ $listTools->aset->kode_aset . " - " . $listTools->aset->tool->nama . " " . $listTools->aset->tool->merk . " " . $listTools->aset->tool->model }}</p>
+                        <input type="hidden" name="id_list_tools[]" value={{ $listTools->id }}>
                         <div class="form-group">
                             <label>Status kondisi</label>
-                            <select class="form-control @error('status_kondisi') is-invalid @enderror" name="status_kondisi" required>
+                            <select class="form-control @error('status_kondisi') is-invalid @enderror" name="status_kondisi[]" required>
                                 <option value="Tidak ada kerusakan">Tidak ada kerusakan</option>
                                 <option value="Ada kerusakan">Ada kerusakan</option>
                             </select>
+                            @error('status_kondisi')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Deskripsi</label>
-                            <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" required></textarea>
+                            <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi[]" required></textarea>
+                            @error('deskripsi')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 @endforeach
             </div>
+            <div class="modal-footer">
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">Kembali</a>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
         </form>
     </div>
+@stop
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            $('#inputGudang').select2();
+        })
+    </script>
 @stop
