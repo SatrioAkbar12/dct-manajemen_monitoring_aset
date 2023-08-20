dd38826fef2a92cb1924611f910b60e2e8369db9
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-20 14:37:43 +0700

    update riwayat peminjaman tool index

--- resources/views/riwayatPeminjamanTool/index.blade.php
+++ resources/views/riwayatPeminjamanTool/index.blade.php
@@ -9,38 +9,42 @@
 @section('content')
     <p>Semua data tentang riwayat peminjaman tools</p>
 
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead class="text-center">
-                <tr>
-                    <th>Tanggal Waktu Peminjaman</th>
-                    <th>Tanggal Waktu Kembali</th>
-                    <th>Peminjam</th>
-                    <th>Tools</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($data_riwayat_peminjaman as $riwayat_peminjaman)
-                    <tr>
-                        <td class="text-center">{{ $riwayat_peminjaman->tanggal_waktu_pinjam }}</td>
-                        <td class="text-center">{{ $riwayat_peminjaman->tanggal_waktu_kembali }}</td>
-                        <td>{{ $riwayat_peminjaman->user->nama }}</td>
-                        <td>
-                            <ul>
-                                @foreach ($riwayat_peminjaman->listTools as $list_tools)
-                                    <li>{{ $list_tools->aset->kode_aset . " - " . $list_tools->aset->tool->nama . " " .  $list_tools->aset->tool->merk . " " . $list_tools->aset->tool->model }}</li>
-                                @endforeach
-                            </ul>
-                        </td>
-                        <td class="text-center">
-                            @can('riwayatPeminjamanTools.detail')
-                                <a href="{{ route('riwayatPeminjamanTools.detail', $riwayat_peminjaman->id) }}" class="btn btn-info">Detail</a>
-                            @endcan
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="text-center">
+                        <tr>
+                            <th>Tanggal Waktu Peminjaman</th>
+                            <th>Tanggal Waktu Kembali</th>
+                            <th>Peminjam</th>
+                            <th>Tools</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data_riwayat_peminjaman as $riwayat_peminjaman)
+                            <tr>
+                                <td class="text-center">{{ $riwayat_peminjaman->tanggal_waktu_pinjam }}</td>
+                                <td class="text-center">{{ $riwayat_peminjaman->tanggal_waktu_kembali }}</td>
+                                <td>{{ $riwayat_peminjaman->user->nama }}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($riwayat_peminjaman->listTools as $list_tools)
+                                            <li>{{ $list_tools->aset->kode_aset . " - " . $list_tools->aset->tool->nama . " " .  $list_tools->aset->tool->merk . " " . $list_tools->aset->tool->model }}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td class="text-center">
+                                    @can('riwayatPeminjamanTools.detail')
+                                        <a href="{{ route('riwayatPeminjamanTools.detail', $riwayat_peminjaman->id) }}" class="btn btn-info">Detail</a>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @stop
