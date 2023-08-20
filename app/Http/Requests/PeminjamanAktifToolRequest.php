4f94e1fe0d3100c92de6b78d5482d881e33695fb
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-20 09:36:48 +0700

    update peminjaman aktif tools request

--- app/Http/Requests/PeminjamanAktifToolRequest.php
+++ app/Http/Requests/PeminjamanAktifToolRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
 
 class PeminjamanAktifToolRequest extends FormRequest
 {
@@ -23,11 +24,25 @@ class PeminjamanAktifToolRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'user' => 'required|integer|exists:\App\Models\User,id',
-            'tools' => 'required|array',
-            'tanggal_waktu_pinjam' => 'required|date',
-            'target_tanggal_waktu_kembali' => 'required|date',
-        ];
+        if(Route::currentRouteName() == 'peminjamanAktifTools.store') {
+            return [
+                'user' => 'required|integer|exists:\App\Models\User,id',
+                'tools' => 'required|array',
+                'tanggal_waktu_pinjam' => 'required|date',
+                'target_tanggal_waktu_kembali' => 'required|date',
+            ];
+        }
+        elseif(Route::currentRouteName() == 'peminjamanAktifTools.update') {
+            return [
+                'gudang' => 'required|exists:\App\Models\Gudang,id',
+                'id_list_tools' => 'required|array',
+                'id_list_tools.*' => 'required|exists:\App\Models\ListToolsTransaksiPeminjaman,id',
+                'status_kondisi' => 'required|array',
+                'status_kondisi.*' => 'required|in:Tidak ada kerusakan,Ada kerusakan',
+                'deskripsi' => 'required|array',
+                'deskripsi.*' => 'required',
+            ];
+        }
+
     }
 }
