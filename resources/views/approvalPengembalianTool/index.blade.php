2c592d3f1a2684fecf213943ad22ca0cf8845f56
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-23 16:19:01 +0700

    add view

---
+++ resources/views/approvalPengembalianTool/index.blade.php
@@ -0,0 +1,57 @@
+@extends('adminlte::page')
+
+@section('title', 'Approval Pengembalian Tools')
+
+@section('content_header')
+    <h1>Approval Pengembalian Tools</h1>
+@stop
+
+@section('content')
+    <p>Semua data tentang pengembalian tools yang perlu persetujuan</p>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="text-center">
+                        <tr>
+                            <th>Tanggal Waktu Pinjam</th>
+                            <th>Tanggal Waktu Kembali</th>
+                            <th>Peminjam</th>
+                            <th>Tools</th>
+                            <th>Keperluan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data_peminjaman as $peminjaman)
+                            <tr>
+                                <td>{{ $peminjaman->tanggal_waktu_pinjam }}</td>
+                                <td>{{ $peminjaman->tanggal_waktu_kembali }}</td>
+                                <td>{{ $peminjaman->user->nama }}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($peminjaman->listTools as $listTools)
+                                            <li>{{ $listTools->aset->kode_aset . " - " . $listTools->aset->tool->nama . " " . $listTools->aset->tool->merk . " " . $listTools->aset->tool->model }}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td>{{ $peminjaman->keperluan }}</td>
+                                <td class="text-center">
+                                    @can('approvalPengembalianTools.review')
+                                        <a href="{{ route('approvalPengembalianTools.review', $peminjaman->id) }}" class="btn btn-info">Review</a>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <br>
+            <div class="d-flex justify-content-center">
+                {{ $data_peminjaman->links() }}
+            </div>
+        </div>
+    </div>
+@stop
