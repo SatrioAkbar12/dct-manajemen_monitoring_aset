a54b5ffa84f3fc0c8c787d28b93f38df100fe0af
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-24 10:05:18 +0700

    add geolocation and foto kondisi tool on riwayat peminjaman tool

--- resources/views/riwayatPeminjamanTool/detail.blade.php
+++ resources/views/riwayatPeminjamanTool/detail.blade.php
@@ -60,9 +60,32 @@
                     <p>: {{ $data_riwayat_peminjaman->gudang->nama }}</p>
                 </div>
             </div>
-        </div>
-        <div class="card-footer">
-            <a href="{{ url()->previous() }}" class="btn btn-secondary">Kembali</a>
+            <div class="row">
+                <div class="col-4 col-md-2">
+                    <p>Lokasi peminjaman</p>
+                </div>
+                <div class="col-8 col-md-10">
+                    <p>:</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <iframe width="100%" height="250px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q={{ $data_riwayat_peminjaman->geolocation_pinjam }}&z=15&output=embed"></iframe>
+                        <div class="card-body">
+                            <h6 class="text-center">Lokasi pinjam</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <iframe width="100%" height="250px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q={{ $data_riwayat_peminjaman->geolocation_kembali }}&z=15&output=embed"></iframe>
+                        <div class="card-body">
+                            <h6 class="text-center">Lokasi kembali</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -78,6 +101,7 @@
                             <th>Tools</th>
                             <th>Status Kondisi</th>
                             <th>Deskripsi</th>
+                            <th>Foto Kondisi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -86,11 +110,45 @@
                                 <td>{{ $list_tools->aset->kode_aset . " - " . $list_tools->aset->tool->nama . " " .  $list_tools->aset->tool->merk . " " . $list_tools->aset->tool->model }}</td>
                                 <td class="text-center">{{ $list_tools->kondisiToolsTransaksiPeminjaman->status_kondisi }}</td>
                                 <td>{{ $list_tools->kondisiToolsTransaksiPeminjaman->deskripsi }}</td>
+                                <td class="text-center">
+                                    <button type="button" class="mx-2 my-1 btn btn-info" data-toggle="modal" data-target="#modalFotoKondisiSebelum{{ $list_tools->id }}">Sebelum</button>
+                                    <button type="button" class="mx-2 my-1 btn btn-info" data-toggle="modal" data-target="#modalFotoKondisiSesudah{{ $list_tools->id }}">Sesudah</button>
+                                </td>
                             </tr>
+
+                            <div class="modal fade" id="modalFotoKondisiSebelum{{ $list_tools->id }}" role="dialog">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Foto Kondisi Sebelum</h4>
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img class="img-fluid" src="{{ url('storage/' . $list_tools->kondisiToolsTransaksiPeminjaman->foto_sebelum) }}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="modal fade" id="modalFotoKondisiSesudah{{ $list_tools->id }}" role="dialog">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Foto Kondisi Sesudah</h4>
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img class="img-fluid" src="{{ url('storage/' . $list_tools->kondisiToolsTransaksiPeminjaman->foto_sesudah) }}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+
+    <a href="{{ url()->previous() }}" class="btn btn-secondary mb-5">Kembali</a>
 @stop
