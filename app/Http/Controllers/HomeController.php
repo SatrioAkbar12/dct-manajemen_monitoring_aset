7c58b16fdd71ee2f5dc799c3d992f2c6fdef5722
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-08-29 11:18:36 +0700

    add view, function controller, and route first login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FirstLoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $auth = Auth::user();
+
+        if($auth->first_login == 1) {
+            return view('renewPassword');
+        }
+
+        return view('firstLogin');
+    }
+
+    public function firstLogin(FirstLoginRequest $request) {
+        $auth = Auth::id();
+
+        User::where('id', $auth)->update([
+            'password' => Hash::make($request->password),
+            'first_login' => 0
+        ]);
+
+        Alert::success('Tersimpan!', 'Berhasil memperbarui password');
+
+        return redirect(route('home'));
     }
 }
