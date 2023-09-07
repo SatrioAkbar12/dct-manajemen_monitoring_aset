50f87511147c369f1cdbd3d374da86bb55fb8751
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-09-07 16:24:47 +0700

    add peminjaman aktif kendaraan notification

---
+++ app/Notifications/PeminjamanAktifKendaraanNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramMessage;
+
+class PeminjamanAktifKendaraanNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $transaksi_peminjaman;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($transaksi)
+    {
+        $this->transaksi_peminjaman = $transaksi;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['telegram'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toTelegram($notifiable)
+    {
+        $notif = TelegramMessage::create()
+            ->to($notifiable)
+            ->content(" ! ! ! ! !     REMINDER     ! ! ! ! ! \n\n")
+            ->line("*Peminjaman Kendaraan : *\n")
+            ->line("Peminjam : " . $this->transaksi_peminjaman->user->nama)
+            ->line("Keperluan : " . $this->transaksi_peminjaman->keperluan)
+            ->line("Lokasi tujuan : " . $this->transaksi_peminjaman->lokasi_tujuan )
+            ->line("Jenis Kendaraan : " . $this->transaksi_peminjaman->kendaraan->jenisKendaraan->nama)
+            ->line("Kendaraan : " . $this->transaksi_peminjaman->kendaraan->nopol . " - " . $this->transaksi_peminjaman->kendaraan->merk . " " . $this->transaksi_peminjaman->kendaraan->tipe . " " . $this->transaksi_peminjaman->kendaraan->warna)
+            ->line("\n*Telah melewati tanggal pengembalian kendaraan*\n\n_Terima kasih_");
+
+        return $notif;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
