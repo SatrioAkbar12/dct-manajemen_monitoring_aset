fc215f8f79db0c3eef8bf25d91a190ae9c4152e2
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-09-08 10:59:11 +0700

    add telegram data controller, request, views, and route

---
+++ app/Http/Requests/TelegramDataRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class TelegramDataRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'id_telegram' => [
+                'nullable',
+                Rule::requiredIf($this->tipe == 'group'),
+                'regex:/-[0-9]+/i',
+                'unique:\App\Models\TelegramData,id_telegram',
+            ],
+            'username' => 'nullable|unique:\App\Models\TelegramData,username|regex:/@[A-Za-z]+/i',
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        if($this->id_telegram == '') {
+            $this->merge([
+                'id_telegram' => null,
+            ]);
+        }
+
+        if($this->username == '') {
+            $this->merge([
+                'username' => null,
+            ]);
+        }
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        Alert::error('Gagal tersimpan!', 'Gagal menyimpan data');
+        throw (new ValidationException($validator))
+                    ->errorBag($this->errorBag)
+                    ->redirectTo($this->getRedirectUrl());
+    }
+
+
+}
