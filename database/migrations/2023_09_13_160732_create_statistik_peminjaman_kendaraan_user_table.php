ff1bc649c83ec676e6e5b7934526aa36c05bed44
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-09-13 16:49:56 +0700

    add statistik peminjaman kendaraan user model and migration

---
+++ database/migrations/2023_09_13_160732_create_statistik_peminjaman_kendaraan_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistik_peminjaman_kendaraan_user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_kendaraan');
+            $table->unsignedInteger('jumlah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistik_peminjaman_kendaraan_user');
+    }
+};
